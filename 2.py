a536d9551cb0b097202170238aa6082dc13b3ce6
K0tok <bodrov.2005@inbox.ru>
2025-03-27 11:46:05 +0500

    27.03.25

--- 2.py
+++
@@ -1,8 +0,0 @@
-a = input()
-b = int(input())
-c = int(input())
-d = int(input())
-
-print("================Чек================")
-print("Товар:" + a.rjust(29))
-print("Цена:" + (str(c) + "кг * " + str(b) + "руб/кг").rjust(30))
